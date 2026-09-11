d8540e32c22290731393ba2b5121a8ed6bfc31ff
lucasviniciusgarcia <lucasviniciusgarcia2006@gmail.com>
2026-09-10 22:01:13 -0300

    Adicionando funcionalidades
    Adicionamos um botão onde iremos adicionar novas tarefas em nossa caixa de tarefas.

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const tarefa = document.getElementById("tarefa");
 const adicionar = document.getElementById("adicionar");
-adicionar.addEventListener("click", function() {const textoTarefa = tarefa.value;
-  });
+const listaTarefas = document.getElementById("listaTarefas");
+
+adicionar.addEventListener("click", function() {
+  const textoTarefa = tarefa.value;
+ const novaTarefa = document.createElement("li");
+novaTarefa.textContent = textoTarefa;
+  listaTarefas.appendChild(novaTarefa);
+});
