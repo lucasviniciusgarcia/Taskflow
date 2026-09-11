353b46ad38b9b99c96cb57c11c045b9a96825751
lucasviniciusgarcia <lucasviniciusgarcia2006@gmail.com>
2026-09-11 08:58:27 -0300

    Adicionamos uma modificação após inserirmos uma tarefa
    No caso após adicionar uma tarefa o campo continuava com a tarefa escrita, então fizemos uma modificação para ficar em branco o campo após a adição da tarefa.

--- script.js
+++ script.js
@@ -8,4 +8,5 @@ adicionar.addEventListener("click", function(event) {
  const novaTarefa = document.createElement("li");
 novaTarefa.textContent = textoTarefa;
   listaTarefas.appendChild(novaTarefa);
+  tarefa.value = "";
 });
