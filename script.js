d40159cccfc274063ef70c17cc5a2e20ad36ac82
lucasviniciusgarcia <lucasviniciusgarcia2006@gmail.com>
2026-09-11 09:04:50 -0300

    Retornamos ao código para deixar o campo em branco
    Após os testes feitos, não conseguimos manter o campo com o sinal de alerta após não funcionar a função para deixa´lo em branco, nesse caso vamos tentar forçar a página a recarregar, pois o código fonte está salvo no outro PC.

--- script.js
+++ script.js
@@ -8,5 +8,5 @@ adicionar.addEventListener("click", function(event) {
  const novaTarefa = document.createElement("li");
 novaTarefa.textContent = textoTarefa;
   listaTarefas.appendChild(novaTarefa);
-  alert("Funcionou!");
+  tarefa.value = "";
 });
