02f2e5055817a52d6f4b1792ce7bb63953c81e08
lucasviniciusgarcia <lucasviniciusgarcia2006@gmail.com>
2026-09-10 22:12:10 -0300

    Adicionamos event
    QUando fazemos isso estamos tentando impedir de o evento recarregar a página automaticamente aconteça.

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const tarefa = document.getElementById("tarefa");
 const adicionar = document.getElementById("adicionar");
 const listaTarefas = document.getElementById("listaTarefas");
 
-adicionar.addEventListener("click", function() {
+adicionar.addEventListener("click", function(event) {
+  event.preventDefault();
   const textoTarefa = tarefa.value;
  const novaTarefa = document.createElement("li");
 novaTarefa.textContent = textoTarefa;
